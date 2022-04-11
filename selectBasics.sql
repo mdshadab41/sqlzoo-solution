c4fb87dffcc802ece0cfc7911512e66ee8452883
Md Shadab <97763170+mdshadab41@users.noreply.github.com>
2022-04-11 18:38:56 +0530

    Add files via upload

---
+++ selectBasics.sql
@@ -0,0 +1,34 @@
+/* Section - First
+    SELECT Basics
+	*/
+				
+--#1
+
+/*Modify it to show the population of Germany */	
+ 
+SELECT population FROM world
+  WHERE name = 'Germany'
+  
+  --#2
+
+/*Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.*/
+	
+ 
+SELECT name, population FROM world
+  WHERE name IN ('Sweden','Norway', 'Denmark');
+  
+  
+  
+   --#3
+	
+ 
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
+  
+  
+  
+  
+  
+  
+  
+  
